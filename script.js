d61daa37bf0a5347335cde3d848d2ea953091a3d
christina <chrstnpet@gmail.com>
2025-11-27 01:13:03 +0200

    Changed function to change the text depending on if it's Sunday or not

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(function() {
     const isSunday = new Date().getDay() === 0; // getDay() returns 0 for Sunday
 
-    $('h1').css("color", isSunday ? "green" : "blue");
+    $('h1').html(isSunday ? "It is Sunday" : "It is not Sunday").css("color", isSunday ? "green" : "blue");
 });
